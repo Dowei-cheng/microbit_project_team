5a2dc1074e9b652387ae47e38911354dc448cbc6
Dowei-cheng <146905768+Dowei-cheng@users.noreply.github.com>
2023-10-14 16:55:03 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,8 @@ basic.forever(function () {
     if (huskylens.isLearned(1)) {
         if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultArrow)) {
             X1 = huskylens.readeArrow(1, Content2.xOrigin)
-            if (X1 < 140) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 0, 0)
+            if (X1 < 135) {
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 60, 10)
                 basic.showLeds(`
                     . . # . .
                     . . # # .
@@ -16,8 +16,8 @@ basic.forever(function () {
                     . . # . .
                     `)
             }
-            if (X1 >= 140 && X1 < 150) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 0, 0)
+            if (X1 >= 135 && X1 < 145) {
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 60, 40)
                 basic.showLeds(`
                     . . . . .
                     . . # . .
@@ -26,8 +26,8 @@ basic.forever(function () {
                     . . . . .
                     `)
             }
-            if (X1 >= 150 && X1 < 160) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 0, 0)
+            if (X1 >= 150 && X1 < 170) {
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 60, 60)
                 basic.showLeds(`
                     . . # . .
                     . # # # .
@@ -36,8 +36,8 @@ basic.forever(function () {
                     . . . . .
                     `)
             }
-            if (X1 >= 160 && X1 < 180) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 0, 0)
+            if (X1 >= 175 && X1 < 185) {
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 40, 60)
                 basic.showLeds(`
                     . . . . .
                     . . # . .
@@ -46,8 +46,8 @@ basic.forever(function () {
                     . . . . .
                     `)
             }
-            if (X1 >= 180) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 0, 0)
+            if (X1 >= 185) {
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 10, 60)
                 basic.showLeds(`
                     . . # . .
                     . # # . .
