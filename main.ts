73a602adde9ee7754768f6650978c08cc583667d
Dowei-cheng <146905768+Dowei-cheng@users.noreply.github.com>
2023-10-14 18:52:47 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,55 +6,22 @@ basic.forever(function () {
     if (huskylens.isLearned(1)) {
         if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultArrow)) {
             X1 = huskylens.readeArrow(1, Content2.xOrigin)
+            led.stopAnimation()
             if (X1 < 135) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 60, 10)
-                basic.showLeds(`
-                    . . # . .
-                    . . # # .
-                    . . # # #
-                    . . # # .
-                    . . # . .
-                    `)
-            }
-            if (X1 >= 135 && X1 < 145) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 60, 40)
-                basic.showLeds(`
-                    . . . . .
-                    . . # . .
-                    . . # # .
-                    . . # . .
-                    . . . . .
-                    `)
-            }
-            if (X1 >= 150 && X1 < 170) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 60, 60)
-                basic.showLeds(`
-                    . . # . .
-                    . # # # .
-                    # # # # #
-                    . . . . .
-                    . . . . .
-                    `)
-            }
-            if (X1 >= 175 && X1 < 185) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 40, 60)
-                basic.showLeds(`
-                    . . . . .
-                    . . # . .
-                    . # # . .
-                    . . # . .
-                    . . . . .
-                    `)
-            }
-            if (X1 >= 185) {
-                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 10, 60)
-                basic.showLeds(`
-                    . . # . .
-                    . # # . .
-                    # # # . .
-                    . # # . .
-                    . . # . .
-                    `)
+                led.plot(0, 0)
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 0, 65)
+            } else if (X1 >= 135 && X1 < 145) {
+                led.plot(1, 0)
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 30, 40)
+            } else if (X1 >= 150 && X1 < 170) {
+                led.plot(2, 0)
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 50, 50)
+            } else if (X1 >= 175 && X1 < 185) {
+                led.plot(3, 0)
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 40, 30)
+            } else if (X1 >= 185) {
+                led.plot(4, 0)
+                mbit_小車類.CarCtrlSpeed2(mbit_小車類.CarState.Car_Run, 65, 0)
             }
         }
     }
